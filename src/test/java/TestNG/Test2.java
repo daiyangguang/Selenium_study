88e3fe8e11f4a7dcb5de9665a0fd1d1991f879a6
liuliu <daiyangguang7@163.com>
2017-12-03 22:59:55 +0800

    add 总结

--- src/test/java/TestNG/Test2.java
+++ src/test/java/TestNG/Test2.java
@@ -3,7 +3,11 @@ package TestNG;
 import org.testng.annotations.Test;
 
 import junit.framework.Assert;
-
+/**
+ * 套件测试可以让我们通过运行testng.xml一次运行多个测试类 
+ * @author Administrator
+ *
+ */
 public class Test2 {
 	String message = "Bolo";
 	MessageUtil messageUtil = new MessageUtil(message);
