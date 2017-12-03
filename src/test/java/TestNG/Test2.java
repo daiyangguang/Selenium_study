66fc998ff6bbc3abbd044f5f2386308b3791a45c
liuliu <daiyangguang7@163.com>
2017-12-03 23:00:19 +0800

    add 总结

--- src/test/java/TestNG/Test2.java
+++ src/test/java/TestNG/Test2.java
@@ -4,7 +4,7 @@ import org.testng.annotations.Test;
 
 import junit.framework.Assert;
 /**
- * 套件测试可以让我们通过运行testng.xml一次运行多个测试类 
+ * 套件可以让我们通过运行testng.xml一次运行多个测试类 
  * @author Administrator
  *
  */
